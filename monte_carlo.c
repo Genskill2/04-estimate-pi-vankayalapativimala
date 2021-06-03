939532ed7c51b3451c25c18e4fe7317b51236e5f
vankayalapativimala <84719756+vankayalapativimala@users.noreply.github.com>
2021-06-03 21:36:24 +0530

    monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -30,23 +30,7 @@ float pi0;
   }
 
     
-  for (int i=2000; i<5000; i++) {
-	  float pi0;
-  float pi1;
-  
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
-  printf("%f %f\n", pi0, pi1);
-  
-  if (pi0 == pi1) {
-      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
-      abort();
-    }
-
-  if (fabs(pi0 - pi1) > 0.05) {
-      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
-      abort();
-  }
+ 
 	  for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
