5f82e5a4e56715b8d05e3214f1d1ec81c8a405c0
vankayalapativimala <84719756+vankayalapativimala@users.noreply.github.com>
2021-06-03 21:30:31 +0530

    monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -12,7 +12,7 @@ float frandom() {
 }
 
 int main(void) {
-  float pi0;
+float pi0;
   float pi1;
   
   pi0 = mc_pi(25000);
@@ -31,6 +31,23 @@ int main(void) {
 
     
   for (int i=2000; i<5000; i++) {
+	  float pi0;
+  float pi1;
+  
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+  
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
+
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
+	  for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
@@ -47,13 +64,13 @@ float mc_pi(int count)
 		float x = frandom();
 		float y = frandom();
 		if(sqrt(x*x+y*y)<1)
-		{
+                  {
 			circount++;
-		}
-	}
-	float area = ((4*circount)/count);
-	return area;
+	          }
+       }
+         float area = ((4*circount)/count);
+         return area;
 }
-
-
+    
+ 
 
