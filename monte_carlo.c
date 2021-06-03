25eb134de10461d231813bc9aab377aaffe419e9
vankayalapativimala <84719756+vankayalapativimala@users.noreply.github.com>
2021-06-03 16:27:47 +0530

    monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,22 @@ int main(void) {
     }
   }
 }
+float mc_pi(int count)
+{
+	float circount=0;
+	for(int i=0;i<count;i++)
+	{
+		
+		float x = frandom();
+		float y = frandom();
+		if(sqrt(x*x+y*y)<1)
+		{
+			circount++;
+		}
+	}
+	float area = ((4*circount)/count);
+	return area;
+}
 
 
 
